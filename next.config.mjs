6e3c096c7d923ad8896087143483e00a0fefaefb
justinsilvestre <justinsilvestre@gmail.com>
2024-04-09 12:33:21 +0200

    Set up pages site

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",  // <=== enables static exports
+  reactStrictMode: true,
+  basePath: '/canvas'
+};
 
-export default nextConfig;
+module.exports = nextConfig;
